73e7eca5f0de53edcd992cdd4c52d50b8de701d1
lizardcat <alex.raza.ke@gmail.com>
2025-08-12 00:07:27 +0300

    Adding routing for dashboard 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Layout } from './components/Layout'
 import { Home } from './components/Home'
 import { Dashboard01 } from './components/dashboards/Dashboard01'
 import { Dashboard02 } from './components/dashboards/Dashboard02'
+import { Dashboard03 } from './components/dashboards/Dashboard03'
 import './index.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/dashboard-01" element={<Dashboard01 />} />
             <Route path="/dashboard-02" element={<Dashboard02 />} />
+            <Route path="/dashboard-03" element={<Dashboard03 />} />
             {/* Add more dashboard routes as you build them */}
             {/* <Route path="/dashboard-02" element={<Dashboard02 />} /> */}
             </Routes>
