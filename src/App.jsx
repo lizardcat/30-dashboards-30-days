67cad156cb1bc9f38c303422e2b6ec1369c8b0df
lizardcat <alex.raza.ke@gmail.com>
2025-08-13 22:46:10 +0300

    Fixing routing for dashboard 4

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             <Route path="/dashboard-01" element={<Dashboard01 />} />
             <Route path="/dashboard-02" element={<Dashboard02 />} />
             <Route path="/dashboard-03" element={<Dashboard03 />} />
-            <Route path="/dashboard-04" element={<Dashboard03 />} />
+            <Route path="/dashboard-04" element={<Dashboard04 />} />
             {/* Add more dashboard routes as you build them */}
             </Routes>
         </Layout>
